4b1f394d828ac556d691742648d1bcb6b471fe7a
sooteak <kids.st2001@gmail.com>
2023-01-10 15:52:13 +0800

    hi

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class ArrayStack{
+    //top maxsize stack
+    private int top = -1;
+    private int maxSize;
+    private int[] stack;
+
+    //constructor
+    public ArrayStack(int maxSize) {
+        this.maxSize = maxSize;
+        stack = new int[this.maxSize];
+    }
+    
+    //isEmpty isFull push pull
+    
+    
+}
